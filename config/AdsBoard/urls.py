49bc46bd72c455638898a4e34f416fcfa2c44de7
Roman Nevzorov <pomkanevzorov@gmail.com>
2023-05-10 15:11:05 +0600

    Added ads_list page: view, url and template.

--- config/AdsBoard/urls.py
+++ config/AdsBoard/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from AdsBoard.views import AdsListView
 
-urlpatterns = []
+
+urlpatterns = [
+    path('ads/', AdsListView.as_view(), name='ads_list'),
+]
--- config/AdsBoard/views.py
+++ config/AdsBoard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import *
 
 
+class AdsListView(ListView):
+    model = Adv
+    template_name = 'AdsBoard/ads_list.html'
+    context_object_name = 'list_of_ads'
+    ordering = '-date_of_creation'
+    paginate_by = 5
+    paginate_orphans = 1
