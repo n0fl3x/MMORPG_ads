3a50720ab986d3e7b8af7a18cef528b6e67b6a50
Roman Nevzorov <pomkanevzorov@gmail.com>
2023-05-13 05:48:33 +0600

    Added ad delete func.

--- config/AdsBoard/urls.py
+++ config/AdsBoard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from AdsBoard.views import AdsListView, AdDetailView
+from AdsBoard.views import AdsListView, AdDetailView, adv_delete
 
 urlpatterns = [
     path('ads/', AdsListView.as_view(), name='ads_list'),
     path('ads/<int:pk>/', AdDetailView.as_view(), name='ad_detail'),
+    path('ads/<int:pk>/ad_del/', adv_delete, name='ad_del'),
+    # path('ads/<int:pk>/repl_del', repl_delete, name='repl_del'),
 ]
--- config/AdsBoard/views.py
+++ config/AdsBoard/views.py
@@ -1,4 +1,6 @@
 from pprint import pprint
+
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 
 from .models import *
@@ -12,8 +14,39 @@ class AdsListView(ListView):
     paginate_by = 5
     paginate_orphans = 1
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        pprint(context)
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        pprint(queryset)
+        return queryset
+
 
 class AdDetailView(DetailView):
     model = Adv
     template_name = 'AdsBoard/ad_detail.html'
     context_object_name = 'ad'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        pprint(context)
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        pprint(queryset)
+        return queryset
+
+
+def adv_delete(request, pk):
+    Adv.objects.get(id=pk).delete()
+    return redirect(to='ads_list')
+
+
+# def repl_delete(request, pk):
+#     ad = Adv.objects.get(id=pk)
+#     # ad.replies_to_adv.get()    ???????????????????
+#     return redirect(request.META.get('HTTP_REFERER'))
